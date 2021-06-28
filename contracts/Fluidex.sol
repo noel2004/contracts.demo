1f40890e00a9a56c2dd56a85b55c71858a271d99
HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>
2021-06-28 22:02:08 +0800

    add constructor and GENESIS_ROOT  (#3)

--- contracts/Fluidex.sol
+++ contracts/Fluidex.sol
@@ -9,7 +9,13 @@ contract FluidexDemo {
       Verified
    }
 
+   uint256 GENESIS_ROOT;
+   mapping(uint256 => uint256) public state_roots;   
    mapping(uint256 => BlockState) public block_states;
+
+   constructor(uint256 _genesis_root) public {
+      GENESIS_ROOT = _genesis_root;
+   }
 
    function get_block_state_by_block_id(uint256 block_id) public returns (BlockState) {
       return block_states[block_id];
