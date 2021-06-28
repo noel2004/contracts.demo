0df69e68aad51bd199dc65c4547ebae10d37b6e8
HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>
2021-06-28 22:00:03 +0800

    feat: add get_block_state_by_block_id (#2)

--- contracts/Fluidex.sol
+++ contracts/Fluidex.sol
@@ -10,4 +10,8 @@ contract FluidexDemo {
    }
 
    mapping(uint256 => BlockState) public block_states;
+
+   function get_block_state_by_block_id(uint256 block_id) public returns (BlockState) {
+      return block_states[block_id];
+   }
 }
