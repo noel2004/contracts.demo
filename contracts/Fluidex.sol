31b3810a5a1c9e33be8282425eb49126ab15931f
HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>
2021-06-28 22:10:08 +0800

    feat: add state check for submit_block (#4)

--- contracts/Fluidex.sol
+++ contracts/Fluidex.sol
@@ -17,7 +17,26 @@ contract FluidexDemo {
       GENESIS_ROOT = _genesis_root;
    }
 
-   function get_block_state_by_block_id(uint256 block_id) public returns (BlockState) {
-      return block_states[block_id];
+   function get_block_state_by_block_id(uint256 _block_id) public returns (BlockState) {
+      return block_states[_block_id];
+   }
+
+   function submit_block(
+      uint256 _block_id,
+      uint256[] memory _public_inputs,
+      uint256[] memory _serialized_proof
+   ) public returns (bool) {
+      // _public_inputs[0] is previous_state_root
+      // _public_inputs[1] is new_state_root
+      require(_public_inputs.length >= 2);
+      if (_block_id == 0) {
+         assert(_public_inputs[0] == GENESIS_ROOT);
+      } else {
+         assert(_public_inputs[0] == state_roots[_block_id-1]);         
+      }
+
+      // TODO: state transistion & proof verification
+
+      return true;
    }
 }
