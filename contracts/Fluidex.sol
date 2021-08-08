879fd983ecd79899f4b235b4684e9a4fea2fa737
HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>
2021-08-09 06:40:14 +0800

    fix: add more restrictions to submitBlock (#20)

--- contracts/Fluidex.sol
+++ contracts/Fluidex.sol
@@ -139,12 +139,20 @@ contract FluidexDemo is Events, KeyedVerifier, ReentrancyGuard {
       if (_serialized_proof.length != 0) {
          // TODO: hash inputs and then pass into verifier
          assert(verify_serialized_proof(_public_inputs, _serialized_proof));
+         if (_block_id > 0) {
+            assert(block_states[_block_id-1] == BlockState.Verified);
+         }
+         assert(block_states[_block_id] != BlockState.Verified);
          block_states[_block_id] = BlockState.Verified;
       } else {
          // mark a block as Submitted (Committed) directly, because we may
          // temporarily run out of proving resource.
          // note: Committing a block without a rollback/revert mechanism should
          // only happen in demo version!
+         if (_block_id > 0) {
+            assert(block_states[_block_id-1] != BlockState.Empty);
+         }
+         assert(block_states[_block_id] == BlockState.Empty);
          block_states[_block_id] = BlockState.Submitted;
       }
       state_roots[_block_id] = _public_inputs[1];
